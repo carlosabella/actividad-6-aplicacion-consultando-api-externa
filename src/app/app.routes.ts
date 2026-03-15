fff70c37e115101f75eaab7d136b0f6b4a3b06a9
Carlos Abella <carlosg.abella@gmail.com>
2026-03-15 10:47:04 +0100

    detail component done and started new component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { UserNewComponent } from './components/user-new/user-new.component';
 
 export const routes: Routes = [
     {
         path: "", pathMatch: 'full', redirectTo: "home"
     },
     { path: "home", component: HomeComponent },
+    { path: "newuser", component: UserNewComponent },
     { path: "user/:id", component: UserDetailComponent },
 ];
 
